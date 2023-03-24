280748f691abc94d29c45495d2f3a225d2835fa0
anjarmath <anjarmath@gmail.com>
2023-03-25 06:41:05 +0700

    coba -tensorflow3

--- app.py
+++ app.py
@@ -11,14 +11,18 @@ app = Flask(__name__)
 
 app.config['UPLOAD_FOLDER'] = './img/'
 
-# model = tf.keras.models.load_model('./model_vgg.h5')
-# model.compile(
-#     loss='binary_crossentropy',
-#     optimizer='SGD',
-#     metrics=['accuracy']
-# )
+
 
 target_names = ['COVID','Normal','Viral Pneumonia']
+
+def load_model():
+    model = tf.keras.models.load_model('./model_vgg.h5')
+    model.compile(
+        loss='binary_crossentropy',
+        optimizer='SGD',
+        metrics=['accuracy']
+    )
+    return model
 
 def is_grey_scale(img_path):
     img = Image.open(img_path).convert('RGB')
@@ -42,20 +46,21 @@ def predict():
     image_path = os.path.join(app.config['UPLOAD_FOLDER'], imagefile.filename)
     imagefile.save(image_path)
 
-    # image = load_img(image_path, target_size=(224,224))
-    # if (is_grey_scale(image_path) == True):
-    #     image1 = img_to_array(image)
-    #     image1 = np.expand_dims(image1, axis=0)
-    #     image1 = np.vstack([image1])
-    #     # prediksi = model.predict(image1)
-    #     # skor = np.max(prediksi)
-    #     # classes = np.argmax(prediksi)
-    #     # if skor > 0.9:
-    #     #     hasil = target_names[classes]
-    #     # else:
-    #     hasil='Tidak terdeteksi apapun, periksa gambar Anda'
-    # else:
-    hasil = 'Gambar tidak terdeteksi sebagai citra x-ray'
+    image = load_img(image_path, target_size=(224,224))
+    if (is_grey_scale(image_path) == True):
+        image1 = img_to_array(image)
+        image1 = np.expand_dims(image1, axis=0)
+        image1 = np.vstack([image1])
+        model = load_model()
+        prediksi = model.predict(image1)
+        skor = np.max(prediksi)
+        classes = np.argmax(prediksi)
+        if skor > 0.9:
+            hasil = target_names[classes]
+        else:
+            hasil='Tidak terdeteksi apapun, periksa gambar Anda'
+    else:
+        hasil = 'Gambar tidak terdeteksi sebagai citra x-ray'
 
     return render_template("hasil.html", result=hasil, img=imagefile.filename)
 
